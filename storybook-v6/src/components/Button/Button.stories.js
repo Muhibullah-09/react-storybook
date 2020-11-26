66f171ffbf9940ca124eb0c0363043818207d35c
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-27 03:51:28 +0500

    use args in stories
    with the help of args we can create stories or we can say we can create stories with props

--- storybook-v6/src/components/Button/Button.stories.js
+++ storybook-v6/src/components/Button/Button.stories.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import Button from './Button';
+import React from 'react'
+import Button from './Button'
 
 export default {
-    title: 'form/Button',
-    component: Button
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Button'
+  }
 }
 
-export const Primary = () => < Button variant = 'primary'> Primary </Button>
-export const Secondary = () => < Button variant = 'secondary'> Secondary </Button>
-export const Success = () => < Button variant = 'success'> Success </Button>
-export const Danger = () => < Button variant = 'danger'> Danger </Button>
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
 
 // Small.storyName = 'Small Input'Not support in v6
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: 'primary',
+  // children: 'Primary Args',
+}
+
+export const SecondaryA = Template.bind({})
+SecondaryA.args = {
+  variant: 'secondary',
+  // children: 'Secondary Args',
+}
+
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  // children: 'Long Primary Args',
+}
