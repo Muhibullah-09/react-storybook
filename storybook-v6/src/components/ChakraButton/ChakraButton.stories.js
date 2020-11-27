0f8816e6e2d55066a2b37f587db9a00e361dcb82
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-27 05:26:32 +0500

    Update ChakraButton.stories.js

--- storybook-v6/src/components/ChakraButton/ChakraButton.stories.js
+++ storybook-v6/src/components/ChakraButton/ChakraButton.stories.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from 'react'
 import { Button } from '@chakra-ui/react';
 
 export default {
-    title: 'Chakra/Button',
-    component: Button
+  title: 'Chakra/Button',
+  component: Button,
+  decorators: [(Story) => <div><Story/></div>],
+  argTypes: {
+    variantColor: {control: 'text'},
+    children: {control: 'text'}
+    }
 }
 
-export const Success = () => <Button variantColor='green'>Success</Button>
-export const Danger = () => <Button variantColor='red'>Danger</Button>
+const Template =  args => <Button {...args} />
+
+export const Success = Template.bind({})
+Success.args = {
+  variantColor: 'green',
+  children: 'Success'
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  variantColor: 'red',
+  children: 'Danger'
+}
